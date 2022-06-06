a83d3d5f3ac204121730f44cdb55fa09d6649d37
JuliaGolban <julia.j.shcherban@gmail.com>
2022-06-06 23:23:28 +0300

    изменено под адаптивную графику

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,18 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    //  const expanded = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+    // const expanded = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
     // refs.openModalBtn.setAttribute('aria-expanded', !expanded);
-   
+
     document.body.classList.toggle('modal-is-open');
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
