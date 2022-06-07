48392d213ae6f82c6d0e6472e4c2ea5f69359862
JuliaGolban <julia.j.shcherban@gmail.com>
2022-06-07 21:53:32 +0300

    update scss

--- js/modal.js
+++ js/modal.js
@@ -9,9 +9,6 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    // const expanded = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-    // refs.openModalBtn.setAttribute('aria-expanded', !expanded);
-
     document.body.classList.toggle('modal-is-open');
     refs.modal.classList.toggle('is-hidden');
   }
